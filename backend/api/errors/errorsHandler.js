5403f24b952c4c523adfea3ac5a1a2bfceeac40c
Long.H <longkr.work@gmail.com>
2023-01-23 23:38:17 -0600

    fixed frontend

--- backend/api/errors/errorsHandler.js
+++ backend/api/errors/errorsHandler.js
@@ -4,7 +4,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
  * Express Page response for errors
  */
 function errorsHandler(err, req, res, next) {
-	const { status = 500, message = "Something went wrong!" } = err;
-	res.status(status).json({ error: message });
+    const { status = 500, message = "Something went wrong!" } = err;
+    res.status(status).json({ error: message });
 }
 module.exports = errorsHandler;
