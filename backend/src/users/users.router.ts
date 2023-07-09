6fddcf0a3f3d1e84cb66a08f2f4cb5f0846be10a
Long.H <longkr.work@gmail.com>
2023-07-08 21:08:32 -0500

    build: working on validation helpers

--- backend/src/users/users.router.ts
+++ backend/src/users/users.router.ts
@@ -1,4 +1,12 @@
-import { Router } from "express";
+import { Router } from 'express';
 
-const router: Router = require("express").Router({ mergeParams: true });
+/**
+ * Defines the router for users resouces
+ * @route api/users
+ * @access Public/Private
+ * @type {Router}
+ * @returns {Router}
+ */
+const router: Router = require('express').Router({ mergeParams: true });
+
 module.exports = router;
